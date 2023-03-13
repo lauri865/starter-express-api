4416c34c8a7e219574d8182aca234f12ca5007c9
Lauri <lauri.lehtmaa@gmail.com>
2023-03-13 23:12:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo123!')
+    res.send('Yo12345!')
 })
 app.listen(process.env.PORT || 3000)
